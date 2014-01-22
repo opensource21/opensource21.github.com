98cab291d4ff114b2f94597f6f4bede164074fbd
niels <opensource21@gmail.com>
2014-01-22 11:46:04 +0100

    Cleanup of the urls remove /./

--- update_directory_index.sh
+++ update_directory_index.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-BASE_URL=http://opensource21.github.com/
+BASE_URL=http://opensource21.github.com
 for DIR in $(find ./releases -type d); do
   (
     echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
-    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk -v "baseUrl=${BASE_URL}${DIR}/" '{ printf "<a href=\""baseUrl"%s\">%s</a>\n",$1,$1 }' 
+    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk -v "baseUrl=${BASE_URL}${DIR:1}/" '{ printf "<a href=\""baseUrl"%s\">%s</a>\n",$1,$1 }' 
     echo -e "</pre>\n</body>\n</html>"
   ) > "${DIR}/index.html"
 done
