08d6cff627ca25013db2d64517b8062be443964f
Niels Stargardt <niels.stargardt@ppi.de>
2014-01-25 19:39:45 +0100

    Now integrate the index-creation to the update-process.

--- update_directory_index.sh
+++ update_directory_index.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 BASE_URL=http://opensource21.github.io
-for DIR in $(find ./releases -type d); do
+REPODIR=./releases
+
+java -jar nexus-indexer-3.0.4-cli.jar -r $REPODIR -i $REPODIR/.index -d $REPODIR/.index -n opensource21
+rm releases/.index/*.cfs
+rm releases/.index/segments*
+rm releases/.index/timestamp
+
+for DIR in $(find $REPODIR -type d); do
   (
     echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
     ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk -v "baseUrl=${BASE_URL}${DIR:1}/" '{ printf "<a href=\""baseUrl"%s\">%s</a>\n",$1,$1 }' 
     echo -e "</pre>\n</body>\n</html>"
   ) > "${DIR}/index.html"
 done
+
 git add -A
+
